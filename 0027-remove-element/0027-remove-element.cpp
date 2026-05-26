097092a3367a4486397281db9c9d0254ab9dea41
Eyad Waleed <eyadwaleedabdelfatah@gmail.com>
2026-05-26 14:56:34 +0300

    Time: 0 ms (100%), Space: 11.8 MB (11.13%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,59 +1,14 @@
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int size = 0 ;
-        int endpoint = 0 ;
-    if(nums.empty()){
-        return 0 ;
-    }
-    else{
-            if(nums[nums.size()-1] == val){
-            for(int index = nums.size()-1; index >= 0 ; index --  ){
-                if(nums[index] == val){
-                    endpoint = index-1  ;
-                }
-                else {
-                    break;
-                }
+      int removeElement(vector<int>& nums, int val) {
+        int read = 0, write = 0;
+        while (read < nums.size()) {
+            if (nums[read] != val) {
+                swap(nums[write], nums[read]);
+                write++;
             }
-
+            read++;
         }
-        else{
-            endpoint = nums.size()-1;
-        }
- 
-                cout<<endpoint;
-
-        for(int index = 0 ; index <  nums.size() ; index++ ){
-            
-                if( index > endpoint){
-                    break;
-                }
-            
-            if(nums[index]== val){
-                if(nums[endpoint]== val){
-                    for(int subIndex = endpoint ; subIndex >= 0 ; subIndex-- ){
-                    if(nums[subIndex] == val){
-                         endpoint = subIndex-1  ;
-                       }
-                else {
-                    break;
-                }
-                        
-                    }
-                }
-                   if( index > endpoint){
-                    break;
-                }
-                int switcher = nums[index];
-                nums[index] = nums[endpoint];
-                nums[endpoint] = switcher;
-               endpoint= endpoint-1  ;
-            }
-        }
-              cout<<endpoint;
-        return endpoint+1 ;
-    }
-  
+        return write;
     }
 };
